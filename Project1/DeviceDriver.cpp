99eb4e6001e4cd53de26d7d6c51919c907cb3561
DESKTOP-5FBE16T\User <kimki1117@gmail.com>
2024-06-03 10:57:55 +0900

    Add DeviceDriver

--- Project1/DeviceDriver.cpp
+++ Project1/DeviceDriver.cpp
@@ -1,4 +1,5 @@
 #include "DeviceDriver.h"
+#include "stdexcept"
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) 
     : m_hardware(hardware)
@@ -6,12 +7,28 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware)
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int ret = (int)(m_hardware->read(address));
+
+    for (int i = 0; i < 4; i++) {
+        if (ret == (int)(m_hardware->read(address)))
+            continue;
+        else {
+            throw(std::logic_error("Device return wrong answer"));
+        }
+            
+    }
+    return ret;
 }
 
 void DeviceDriver::write(long address, int data)
 {
+    unsigned char readVal = m_hardware->read(address);
+    if (readVal == 0xff) {
+        m_hardware->write(address, (unsigned char)data);
+    }
+    else {
+        throw(std::logic_error("Device is not empty"));
+    }
     // TODO: implement this method
-    m_hardware->write(address, (unsigned char)data);
+    
 }
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,7 +2,83 @@
 #include "gmock/gmock.h"
 #include "../Project1/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace testing;
+using namespace std;
+
+class DeviceMock : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+class DeviceTestFixture : public Test {
+public:
+	DeviceMock flashMem;
+	DeviceDriver *testDD = new DeviceDriver(&flashMem);
+};
+
+
+TEST_F(DeviceTestFixture, ReadTest) {
+	// Read 를 호출 하면 5번 불려야 한다.
+	EXPECT_CALL(flashMem, read)
+		.Times(5);
+
+	// Device drive 호출
+	testDD->read(0xA);
+}
+
+TEST_F(DeviceTestFixture, ReadTest2) {
+	// Read 를 호출 하면 5번 불려야 한다.
+	// 5번 불렀을 때 모두 같은 값이 return 되도록 한다.
+	EXPECT_CALL(flashMem, read)
+		.WillRepeatedly(Return(0x3));
+
+	// Device drive 호출
+	int actual = testDD->read(0xA);
+	EXPECT_EQ(3, actual);
+}
+
+TEST_F(DeviceTestFixture, ReadTest3) {
+	// Read 를 호출 하면 5번 불려야 한다.
+	// 1번 다른 값이 불리면 exception이 호출 되어야 한다.
+	EXPECT_CALL(flashMem, read(0xA))
+		.Times(2)
+		.WillOnce(Return(0x5))
+		.WillRepeatedly(Return(0x3));
+
+	try {
+		int actual = testDD->read(0xA);
+
+		FAIL();
+	}
+	catch(exception &e) {
+		e.what();
+		//Success
+	}
+}
+
+TEST_F(DeviceTestFixture, WriteTest) {
+	// write 를 호출한다.
+	EXPECT_CALL(flashMem, read)
+		.WillRepeatedly(Return(0xff));
+
+	EXPECT_CALL(flashMem, write)
+		.Times(1);
+
+	testDD->write(0xA, 0xaa);
+}
+
+TEST_F(DeviceTestFixture, WriteTest2) {
+	// write 를 호출한다.
+	EXPECT_CALL(flashMem, read)
+		.WillRepeatedly(Return(0x03));
+
+	try {
+		testDD->write(0xA, 0xaa);
+		FAIL();
+	}
+	catch (exception& e) {
+		e.what();
+		//Success
+	}
 }
